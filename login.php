23df453552f4d00cb0ba0331076f6fa7d78e5bd8
anwar9563 <43861146+anwar9563@users.noreply.github.com>
2020-02-03 13:49:23 +0600

    Lab-2
    HTML,PHP

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Form</title>
+</head>
+<body>
+	<form action="task.php" method="post">
+		<fieldset>
+			<legend><b>Donor Information</b></legend>
+			First name:<br>
+            <input type="text" name="firstname"><br>
+            Last name:<br>
+            <input type="text" name="lastname"><br>
+            Email:<br>
+            <input type="text" name="email"><br>
+            <br><br>
+            <input type="submit" value="Submit">
+  
+        </fieldset>
+    </form>   
+</body>
+</html>
---
+++ task.php
@@ -0,0 +1,10 @@
+<?php
+echo "My name is khan";
+echo "<br>";
+$a=$_POST["email"];
+echo "<br>";
+var_dump($_POST["email"]);
+echo "<br>";
+var_dump($_POST);
+echo $a;
+?>
